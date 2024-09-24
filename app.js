4f87c0b09b878b976321fdd70ed3bfc44f99bc89
SalaiThomas <salaithomas582@gmail.com>
2024-09-24 23:15:58 +0700

    start_with_component

---
+++ app.js
@@ -0,0 +1,17 @@
+const {Component,mount,xml,useState} = owl;
+
+class Counter extends Component{
+    static template = xml`
+    <button t-on-click="increment">
+    Click Me! [<t t-esc="state.value"/>]
+    </button>
+    `;
+
+    state = useState({value:0})
+
+    increment(){
+        this.state.value++;
+    }
+}
+
+mount(Counter,document.body);
